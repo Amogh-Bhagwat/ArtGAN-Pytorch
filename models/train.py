fbf56b770ce97ed97e438a62f4cf58e78cb596f5
Amogh Bhagwat <amoghbhagwat00@gmail.com>
2019-11-19 01:43:35 +0530

    added gpu and save

--- models/train.py
+++ models/train.py
@@ -6,6 +6,8 @@ from data_loader import get_dataset
 from network import Generator, Discriminator, log_sum_exp 
 from torch.nn.functional import one_hot
 import torch.nn as nn
+import matplotlib.pyplot as plt
+import torchvision.utils as vutils
 
 # Parameters
 init_epoch = 0 
@@ -18,14 +20,16 @@ batch_size = 100
 zdim = 100
 n_classes = 23
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 # Dataloader
 folder = "../data/wikiart/"
 file_name = "../data/wikiart_csv/artist_train.csv"
 train_loader = get_dataset(file_name, folder, batch_size)
 
 # Network 
-gen = Generator(in_channels=zdim+n_classes)
-dis = Discriminator(classes=n_classes)
+gen = Generator(in_channels=zdim+n_classes).to(device)
+dis = Discriminator(classes=n_classes).to(device)
 
 d_optimizer = torch.optim.RMSprop(dis.parameters(), lr=lr)
 g_optimizer = torch.optim.RMSprop(gen.parameters(), lr=lr)
@@ -34,13 +38,13 @@ g_optimizer = torch.optim.RMSprop(gen.parameters(), lr=lr)
 for index in range(init_epoch, max_epoch):
 	for i, data in enumerate(train_loader):
 		# Define input variables 
-		x_n = data[0]
-		y = torch.tensor(np.array(data[1], dtype=np.float32))
+		x_n = torch.tensor(np.array(data[0], dtype=np.float32), device=device)
+		y = torch.tensor(np.array(data[1], dtype=np.float32), device=device)
 		batch_size = y.size()[0]
-		z = torch.FloatTensor(batch_size, zdim).uniform_(-1, 1)
+		z = torch.FloatTensor(batch_size, zdim).uniform_(-1, 1).to(device)
 		# iny - one-hot encoded tensor for class labels
-		iny = np.tile(np.eye(n_classes, dtype=np.float32), [int(batch_size / n_classes + 1), 1])[:batch_size, :]
-		gen_inp = torch.cat((z, torch.tensor(iny)), axis=1)
+		iny = torch.tensor(np.tile(np.eye(n_classes, dtype=np.float32), [int(batch_size / n_classes + 1), 1])[:batch_size, :], device=device)
+		gen_inp = torch.cat((z, iny), axis=1)
 
 		pred_n, recons_n = dis.forward(x_n)
 		samples = gen.forward(gen_inp)
@@ -50,7 +54,7 @@ for index in range(init_epoch, max_epoch):
 		lreal = log_sum_exp(pred_n)
 		lfake = log_sum_exp(pred_g)
 		cost_pred_n = nn.CrossEntropyLoss(reduction='mean')(pred_n, y.to(torch.int64))
-		cost_recons_n = torch.mean((recons_n - x_n.to(torch.float32)) ** 2) * 0.5
+		cost_recons_n = torch.mean((recons_n - x_n.to) ** 2) * 0.5
 		cost_recons_g = torch.mean((recons_g - samples) ** 2) * 0.5 
 		cost_dis_n = -torch.mean(lreal) + torch.mean(nn.Softplus()(lreal))
 		cost_dis_gen_fake = torch.mean(nn.Softplus()(lfake))
@@ -69,3 +73,17 @@ for index in range(init_epoch, max_epoch):
 		g_optimizer.step()
 
 		print('Epoch:{}, G_loss={}, D_loss={}'.format(index, cost_gen.item(), cost_dis.item()))
+		
+	# get images to test progress of generator and save generator 
+	if index % save_epoch == 0 or index == max_epoch -1:
+		path = "../saved_models/" + str(index)
+		torch.save(gen.state_dict(), path)
+
+		with torch.no_grad():
+			samples = samples.detach()
+		img_list = vutils.make_grid(samples, nrow=10, padding=2).numpy()
+		plt.imshow(np.transpose(img_list, (1,2,0)), interpolation="nearest")
+		path = "../gen_imgs/" + str(index) + ".png"
+		plt.savefig(path)
+		
+
