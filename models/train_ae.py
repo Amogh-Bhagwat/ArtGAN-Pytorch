2df3af945181c11bd38fa60d95309443f5d536c4
Amogh-Bhagwat <amoghbhagwat00@gmail.com>
2019-11-23 21:31:11 +0530

    bugs fixed

--- models/train_ae.py
+++ models/train_ae.py
@@ -32,7 +32,7 @@ root = "../data/"
 train_loader = get_dataset(root, batch_size)
 # Network 
 gen = Generator(in_channels=zdim+n_classes).to(device)
-dis = Discriminator(classes=n_classes, device=device).to(device)
+dis = Discriminator(classes=n_classes + 1, device=device).to(device) # Additional fake class
 
 d_optimizer = torch.optim.RMSprop(dis.parameters(), lr=lr)
 g_optimizer = torch.optim.RMSprop(gen.parameters(), lr=lr)
@@ -45,30 +45,35 @@ for index in range(init_epoch, max_epoch):
 		# Define input variables 
 		x_n = torch.tensor(np.array(data[0], dtype=np.float32), device=device)
 		y = torch.tensor(np.array(data[1], dtype=np.float32), device=device)
-		y_one_hot = one_hot(y.to(torch.int64), n_classes)
-		batch_size = y.size()[0]
+		y_one_hot = one_hot(y.to(torch.int64), n_classes + 1) # Additional fake class 
+		batch_size = y.size()[0] 
 		z = torch.FloatTensor(batch_size, zdim).uniform_(-1, 1).to(device)
 		# iny - one-hot encoded tensor for class labels
 		iny = torch.tensor(np.tile(np.eye(n_classes, dtype=np.float32), [int(batch_size / n_classes + 1), 1])[:batch_size, :], device=device)
 		gen_inp = torch.cat((z, iny), dim=1)
+		y_fake = torch.tensor(np.full((1, batch_size), n_classes + 1), device=device)
+		y_fake_one_hot = one_hot(y.to(torch.int64), n_classes + 1)
 
 		pred_n, recons_n = dis.forward(x_n)
 		samples = gen.forward(gen_inp)
 		pred_g, recons_g = dis.forward(samples)
 
 		# Define generator and discriminator loss
-		lreal = log_sum_exp(pred_n)
-		lfake = log_sum_exp(pred_g)
-		cost_pred_n = F.binary_cross_entropy(pred_n, y_one_hot.to(torch.float32))
-		cost_recons_n = torch.mean((recons_n - x_n) ** 2) * 0.5
+		# lreal = log_sum_exp(pred_n)
+		# lfake = log_sum_exp(pred_g)
+		cost_pred_n = F.binary_cross_entropy(pred_n, y_one_hot.to(torch.float32)) # cost for real image prediction
+		cost_recons_n = torch.mean((recons_n - x_n) ** 2) * 0.5				      # reconstruction losses 
 		cost_recons_g = torch.mean((recons_g - samples) ** 2) * 0.5 
-		cost_dis_n = -torch.mean(lreal) + torch.mean(nn.Softplus()(lreal))
-		cost_dis_gen_fake = torch.mean(nn.Softplus()(lfake))
-		cost_dis = cost_dis_n + cost_dis_gen_fake + cost_pred_n + cost_recons_n
+		cost_pred_f = F.binary_cross_entropy(pred_g, y_fake_one_hot.to(torch.float32))
+		# cost_dis_n = -torch.mean(lreal) + torch.mean(nn.Softplus()(lreal))
+		# cost_dis_gen_fake = torch.mean(nn.Softplus()(lfake))
+		cost_dis = cost_pred_f + cost_pred_n + cost_recons_n
 
-		cost_dis_g = -torch.mean(lfake) + torch.mean(nn.Softplus()(lfake))
-		cost_pred_g = F.binary_cross_entropy(pred_g, iny)
-		cost_gen = cost_dis_g + cost_pred_g + cost_recons_g
+		# cost_dis_g = -torch.mean(lfake) + torch.mean(nn.Softplus()(lfake))
+		zeros = torch.tensor(np.full((batch_size, 1), 0), device=device)
+		y_fake_loss = torch.cat((iny, zeros.to(torch.float32)), dim=1)
+		cost_pred_g = F.binary_cross_entropy(pred_g, y_fake_loss)   # adversarial loss for generator
+		cost_gen = cost_pred_g + cost_recons_g
 
 		d_optimizer.zero_grad()
 
